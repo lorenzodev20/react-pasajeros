83e29d3d4932c281f5d82654a4f18687ab510c92
Lorenzo Antonio Rojo Garces <lorenzodev2020@gmail.com>
2025-06-13 09:26:14 -0500

    feat: edición y datos informativos - editar pasajero - ajuste a la vista de agregar abono

--- src/pages/EditPassenger.tsx
+++ src/pages/EditPassenger.tsx
@@ -1,35 +1,44 @@
-import React, { useMemo, useState, type ChangeEvent } from 'react'
+import React, { useEffect, useMemo, useState, type ChangeEvent } from 'react'
 import { usePassenger } from '../hooks/usePassenger';
-import type { PassengerPreview } from '../types';
-import { initialPassengerPreview } from '../reducers/passenger-reducer';
+import type { Passenger } from '../types';
 import { usePassengerApi } from '../hooks/usePassengerApi';
 import SuccessMessage from '../components/SuccessMessage';
+import { useParams } from 'react-router';
 
-export default function CreatePassenger() {
+export default function EditPassenger() {
+    const { passengerId } = useParams<{ passengerId: string }>();
     const { state } = usePassenger();
-
-    const [passenger, setPassenger] = useState<PassengerPreview>(initialPassengerPreview);
-
+    const [passenger, setPassenger] = useState<Passenger>(state.passengerDetail);
     const [success, setSuccess] = useState(false);
-
     const [disabled, setDisabled] = useState(false);
+    const { updatePassenger, getPassenger } = usePassengerApi();
 
-    const { store, parsePassenger } = usePassengerApi();
+    useMemo(() => setPassenger(state.passengerDetail), [state.passengerDetail])
 
-    useMemo(() => setPassenger(state.passengerPreview), [state.passengerPreview])
+    useEffect(() => {
+        const loadDetail = async () => {
+            let { passenger } = await getPassenger(Number(passengerId));
+            if (passenger) {
+                setPassenger(passenger)
+            }
+        }
+        loadDetail();
+    }, []);
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
         setPassenger({ ...passenger, [name]: value })
     }
 
+    const handleChangeTextArea = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setPassenger({ ...passenger, notes: e.target.value })
+    }
+
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         // dispatch({ type: 'store-passenger', payload: { passenger } })
         setDisabled(true);
-        await store(parsePassenger(passenger));
-
-        setPassenger(initialPassengerPreview);
+        await updatePassenger(Number(passengerId), passenger);
 
         setSuccess(true);
 
@@ -40,8 +49,11 @@ export default function CreatePassenger() {
 
     return (
         <div className="max-w-md mx-auto bg-white p-6 rounded-lg shadow-md">
-            {success ? (<SuccessMessage>{" Registro exitoso! "}</SuccessMessage>) : ''}
+            {success ? (<SuccessMessage>{" Actualización exitosa! "}</SuccessMessage>) : ''}
             <form className="space-y-5 p-2" onSubmit={handleSubmit}>
+                <div className="space-y-5 p-2">
+                    <p className="text-2xl underline">Edición datos pasajero</p>
+                </div>
                 <div>
                     <label htmlFor="name" className="block text-sm font-medium text-gray-700">Nombres</label>
                     <input
@@ -64,19 +76,44 @@ export default function CreatePassenger() {
                     />
                 </div>
 
+                <div>
+                    <label htmlFor="last_name" className="block text-sm font-medium text-gray-700">Documento de identidad</label>
+                    <input
+                        name="identity"
+                        type="text"
+                        className="mt-1 block w-full border border-gray-300 rounded-md p-2"
+                        value={passenger.identity}
+                        onChange={handleChange}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="last_name" className="block text-sm font-medium text-gray-700">Notas</label>
+                    <textarea
+                        className="mt-1 block w-full border border-gray-300 rounded-md p-2"
+                        name="notes"
+                        onChange={handleChangeTextArea}
+                        value={passenger.notes}
+                    >
+                        {passenger.notes}
+                    </textarea>
+                </div>
+
                 {/* <!-- Botones --> */}
                 <div className="flex justify-between mt-4">
                     <button
                         type="submit"
                         className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 disabled:bg-blue-500"
                         disabled={disabled}
-                    >Registrar</button>
-                    <button
+                    >
+                        Modificar
+                    </button>
+                    {/* <button
                         type="button"
                         className="bg-gray-300 text-black px-4 py-2 rounded hover:bg-gray-400 disabled:bg-gray-200"
-                        onClick={() => setPassenger(initialPassengerPreview)}
+                        onClick={() => navigate(-1)}
                         disabled={disabled}
-                    >Limpiar campos</button>
+                    >Volver</button> */}
                 </div>
             </form>
         </div>
